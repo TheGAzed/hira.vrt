2a5f25217e1c8000e504f2b55e49f8eab63bb6c7
Matej Dedina <gameplay.nano@gmail.com>
2024-05-30 14:05:25 +0200

    Added comments for controllers and changed SettingsController.

--- hiravrt/Controllers/MainController.cs
+++ hiravrt/Controllers/MainController.cs
@@ -4,8 +4,17 @@ using hiravrt.Controllers.Nav;
 namespace hiravrt.Controllers
 {
     public class MainController {
+		/// <summary>
+		/// Game controller that contains all available games for views.
+		/// </summary>
 		public GameController GameC { get; } = new();
+		/// <summary>
+		/// Home controller with mainly addresses used by views.
+		/// </summary>
 		public HomeController HomeC { get; } = new();
+		/// <summary>
+		/// Settings controller used to control settings specidied by user.
+		/// </summary>
 		public SettingsController SettingsC { get; } = new();
 
 		public MainController() {
--- hiravrt/Controllers/Nav/SettingsController.cs
+++ hiravrt/Controllers/Nav/SettingsController.cs
@@ -6,10 +6,10 @@ namespace hiravrt.Controllers.Nav {
 	/// Graph states in settings razor section.
 	/// </summary>
 	public enum GraphState : int {
-		MONOGRAPH           = 0b00,
-		DIGRAPH             = 0b01,
-		DIACRITIC_MONOGRAPG = 0b10,
-		DIACRITIC_DIGRAPH   = 0b11,
+		MONOGRAPH           = 0b00, // MONOGRAPHIC SYLLABLES ENUM
+		DIGRAPH             = 0b01, // DIGRAPHIC SYLLABLES ENUM
+		DIACRITIC_MONOGRAPG = 0b10, // MONOGRAPHIC SYLLABLES WITH DIACRITICS ENUM
+		DIACRITIC_DIGRAPH   = 0b11, // DIGRAPHIC SYLLABLES WITH DIACRITICS ENUM
 	}
 
 	public class SettingsController {
@@ -17,12 +17,39 @@ namespace hiravrt.Controllers.Nav {
 		/// List of all game models that rely on settings' active syllables
 		/// </summary>
 		public List<GameModel> GameModels = [];
-		private readonly Dictionary<GraphState, GraphModel> keyGraphPair;
+		/// <summary>
+		/// Lookup dictionary that returns concrete GraphModel based on GraphState
+		/// </summary>
+		private readonly Dictionary<GraphState, GraphModel> StateGraphPair;
+		/// <summary>
+		/// Returns current set graph in sesttings.
+		/// </summary>
 		public GraphState CurrentGraphState { get; set; } = GraphState.MONOGRAPH;
-		public List<string> AvailableSyllables { get; set; } = [];
+		/// <summary>
+		/// List of all available/active syllables.
+		/// </summary>
+		private List<string> AvailableSyllables = [];
+		/// <summary>
+		/// Default syllables used by every gamemodel.
+		/// </summary>
+		public static readonly List<string> DefaultSyllables = [
+					"\u3042", "\u3044", "\u3046", "\u3048", "\u304a",
+					"\u304b", "\u304d", "\u304f", "\u3051", "\u3053",
+					"\u3055", "\u3057", "\u3059", "\u305b", "\u305d",
+					"\u305f", "\u3061", "\u3064", "\u3066", "\u3068",
+					"\u306a", "\u306b", "\u306c", "\u306d", "\u306e",
+					"\u306f", "\u3072", "\u3075", "\u3078", "\u307b",
+					"\u307e", "\u307f", "\u3080", "\u3081", "\u3082",
+					"\u3084",           "\u3086",           "\u3088",
+					"\u3089", "\u308a", "\u308b", "\u308c", "\u308d",
+					"\u308f",                               "\u3092",
+			"\u3093",
+		];
+
+		public int AvailableSyllablesCount { get { return AvailableSyllables.Count; } }
 
 		public SettingsController() {
-			keyGraphPair = new Dictionary<GraphState, GraphModel>() {
+			StateGraphPair = new Dictionary<GraphState, GraphModel>() {
 				{ GraphState.MONOGRAPH,           new MonographModel(this)          },
 				{ GraphState.DIGRAPH,             new DigraphModel(this)            },
 				{ GraphState.DIACRITIC_MONOGRAPG, new DiacriticMonographModel(this) },
@@ -31,37 +58,60 @@ namespace hiravrt.Controllers.Nav {
 			SetGuesses();
 		}
 
+		/// <summary>
+		/// Adds gamemodel to notify if list of available syllables has changed.
+		/// </summary>
+		/// <param name="model">Concrete gamemodel to notify.</param>
 		public void AddGame(GameModel model) {
 			GameModels.Add(model);
-			model.RemainingSyllables = AvailableSyllables;
+			model.RemainingSyllables = new(AvailableSyllables);
 			model.Reset();
 		}
 
+		/// <summary>
+		/// Gets current concrete graph model set.
+		/// </summary>
+		/// <returns>Current graph model.</returns>
 		public GraphModel GetCurrentGraph() {
-			return keyGraphPair[CurrentGraphState];
+			return StateGraphPair[CurrentGraphState];
 		}
 
+		/// <summary>
+		/// Sets next current graph based on syllable count (MONOGRAPH to DIGRAPH and vice versa)
+		/// </summary>
 		public void NextGraph() {
 			CurrentGraphState = (GraphState)(((int)CurrentGraphState ^ 0b01) & ~0b10);
 		}
 
+		/// <summary>
+		/// Sets next current graph based on diacritic (NON-DIACRITIC MONO/DIGRAPH to DIACRITIC and vice versa)
+		/// </summary>
 		public void NextDiacritic() {
 			CurrentGraphState = (GraphState)((int)CurrentGraphState ^ 0b10);
 		}
 
+		/// <summary>
+		/// Clears AvailableSyllables and adds new active guesses.
+		/// </summary>
 		private void SetGuesses() {
 			AvailableSyllables.Clear();
 
-			foreach (GraphModel m in keyGraphPair.Values) {
-				foreach (string g in m.Guesses) {
-					AvailableSyllables.Add(g);
+			foreach (GraphModel model in StateGraphPair.Values) {
+				foreach (string kana in model.Guesses) {
+					AvailableSyllables.Add(kana);
 				}
 			}
 		}
 
+		/// <summary>
+		/// Resets AvailableSyllables and notifies all subscribed gamemodels that AvailableSyllables have changed by resetting them.
+		/// </summary>
 		public void Notify() {
 			SetGuesses();
-			foreach (GameModel m in GameModels) m.Reset(); 
+			foreach (GameModel m in GameModels) {
+				m.RemainingSyllables = new(AvailableSyllables);
+				m.Reset();
+			}
 		}
 	}
 }
