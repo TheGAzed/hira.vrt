5fb3371a5d8e579b5e6a39664345fa758bbbe7bb
Matej Dedina <gameplay.nano@gmail.com>
2024-06-19 21:29:51 +0200

    added 'true latin form' support for keyboard game mode

--- hiravrt/Models/Game/KeyboardModel.cs
+++ hiravrt/Models/Game/KeyboardModel.cs
@@ -1,12 +1,14 @@
-﻿using System.Linq;
+﻿using System.Globalization;
+using System.Linq;
 using System.Text;
 using static System.Formats.Asn1.AsnWriter;
 
 namespace hiravrt.Models.Game {
 	public class KeyboardModel(LookUp lookUp) : GameModel(1) {
-		private StringBuilder GuessLatin = new StringBuilder();
+		private readonly StringBuilder GuessLatin = new();
 		public string CorrectSyllableGrid = "";
 		public string CorrectSyllableGridAlternative = "";
+		private readonly CultureInfo Culture = new("en-US");
 
 		public override bool IsCorrect(string syllable) {
 			return CorrectSyllableGrid.CompareTo(GuessLatin.ToString() + syllable) == 0 || 
@@ -14,8 +16,8 @@ namespace hiravrt.Models.Game {
 		}
 
 		public bool IsValid(string syllable) {
-			return CorrectSyllableGrid.StartsWith(GuessLatin.ToString() + syllable) ||
-				CorrectSyllableGridAlternative.StartsWith(GuessLatin.ToString() + syllable);
+			return CorrectSyllableGrid.StartsWith(GuessLatin.ToString() + syllable, false, Culture) ||
+				CorrectSyllableGridAlternative.StartsWith(GuessLatin.ToString() + syllable, false, Culture);
 		}
 
 		public override void NextMove(string syllable) {
